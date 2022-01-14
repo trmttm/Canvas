eb9b8806a370eeb5ddf294a5560bf6421f5fdba4
trmttm <96355189+trmttm@users.noreply.github.com>
2022-01-14 21:41:50 +0400

    AddRectangle, not AddTextBox

--- AddRectangle/view.py
+++ AddRectangle/view.py
@@ -3,7 +3,7 @@ from interface_view import ViewABC
 
 def view_factory(app: ViewABC):
     def add_rectangle(view_model: dict):
-        view_model = [{
+        view_model = {
             'width': view_model.get('width'),
             'height': view_model.get('height'),
             'x': view_model.get('x'),
@@ -11,10 +11,8 @@ def view_factory(app: ViewABC):
             'border_color': view_model.get('border_color'),
             'border_width': view_model.get('border_width'),
             'fill': view_model.get('fill'),
-            'text': 'hello',
-            'text_rotation': 0,
             'tags': '',
-        }, ]
-        app.add_text_box(view_model)
+        }
+        app.add_rectangle(view_model)
 
     return add_rectangle
