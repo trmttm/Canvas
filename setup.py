0aaad50caed2645bca73708b639bd1a3c5c15120
trmttm <96355189+trmttm@users.noreply.github.com>
2022-01-17 16:58:54 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='Canvas',
+    version='',
+    packages=['src', 'src.AddLine', 'src.AddText', 'src.MoveLine', 'src.MoveText', 'src.RemoveLine', 'src.RemoveText',
+              'src.AddRectangle', 'src.SetFillColor', 'src.SetLineArrow', 'src.SetLineColor', 'src.SetLineWidth',
+              'src.SetTextColor', 'src.MoveRectangle', 'src.SetBorderColor', 'src.SetBorderWidth',
+              'src.ComplexCommands', 'src.RemoveRectangle', 'src.SetTextFontSize', 'src.ChangeRectangleShape',
+              ],
+    url='',
+    license='',
+    author='STTM',
+    author_email='',
+    description=''
+)
