b0d97152e1fbcba3acc57271eab3e414004e8e94
trmttm <96355189+trmttm@users.noreply.github.com>
2022-02-16 21:36:13 +0400

    Renaming response_model variable

--- complex_commands/instructions.py
+++ complex_commands/instructions.py
@@ -1,16 +1,19 @@
-from use_cases import rm0
-from use_cases import rm2
-from use_cases import rm5
-from use_cases import rm6
-from use_cases import rm9
+from use_cases import rm00
+from use_cases import rm01
+from use_cases import rm02
+from use_cases import rm05
+from use_cases import rm06
+from use_cases import rm09
+from use_cases import rm10
+from use_cases import rm13
 
 
 def get_instructions_add_text_box(shape_id, text, fill_color, xy, wh) -> tuple:
     package_numbers = (0, 5, 6)
     request_models = (
-        rm0(xy, wh, tags=shape_id),
-        rm5(shape_id, fill_color),
-        rm6(xy, text, wh, tags=shape_id),
+        rm00(xy, wh, tags=shape_id),
+        rm05(shape_id, fill_color),
+        rm06(xy, text, wh, tags=shape_id),
     )
     return package_numbers, request_models
 
@@ -18,8 +21,8 @@ def get_instructions_add_text_box(shape_id, text, fill_color, xy, wh) -> tuple:
 def get_instruction_move_left(shape_id, delta) -> tuple:
     package_numbers = (2, 9)
     request_models = (
-        rm2(shape_id, -delta, 0),
-        rm9(shape_id, -delta, 0),
+        rm02(shape_id, -delta, 0),
+        rm09(shape_id, -delta, 0),
     )
     return package_numbers, request_models
 
@@ -27,8 +30,8 @@ def get_instruction_move_left(shape_id, delta) -> tuple:
 def get_instruction_move_right(shape_id, delta) -> tuple:
     package_numbers = (2, 9)
     request_models = (
-        rm2(shape_id, delta, 0),
-        rm9(shape_id, delta, 0),
+        rm02(shape_id, delta, 0),
+        rm09(shape_id, delta, 0),
     )
     return package_numbers, request_models
 
@@ -36,8 +39,8 @@ def get_instruction_move_right(shape_id, delta) -> tuple:
 def get_instruction_move_up(shape_id, delta) -> tuple:
     package_numbers = (2, 9)
     request_models = (
-        rm2(shape_id, 0, -delta),
-        rm9(shape_id, 0, -delta),
+        rm02(shape_id, 0, -delta),
+        rm09(shape_id, 0, -delta),
     )
     return package_numbers, request_models
 
@@ -45,7 +48,17 @@ def get_instruction_move_up(shape_id, delta) -> tuple:
 def get_instruction_move_down(shape_id, delta) -> tuple:
     package_numbers = (2, 9)
     request_models = (
-        rm2(shape_id, 0, delta),
-        rm9(shape_id, 0, delta),
+        rm02(shape_id, 0, delta),
+        rm09(shape_id, 0, delta),
+    )
+    return package_numbers, request_models
+
+
+def get_instructions_remove_all() -> tuple:
+    package_numbers = (1, 10, 13)
+    request_models = (
+        rm01('all', ),
+        rm10('all', ),
+        rm13('all', ),
     )
     return package_numbers, request_models
