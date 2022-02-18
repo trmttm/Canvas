9184dad4a163f259efb4248b9dccd0e949d93d9b
trmttm <96355189+trmttm@users.noreply.github.com>
2022-02-18 12:25:25 +0400

    Update Entities

--- entities/__init__.py
+++ entities/__init__.py
@@ -1,3 +1,7 @@
+from typing import Any
+from typing import Dict
+
+
 class Selection:
     def select(self, shape_id):
         pass
@@ -6,12 +10,20 @@ class Selection:
 class Shapes:
     def __init__(self):
         self._next_shape_id = 0
-        self._data = {}
+        self._data: Dict[Any, dict] = {}
 
     def add_text_box(self, **kwargs) -> int:
         self._next_shape_id += 1
         self._data[self._next_shape_id] = kwargs
         return self._next_shape_id
+
+    @property
+    def shape_ids(self) -> tuple:
+        return tuple(self._data.keys())
+
+    def configure(self, shape_id, **options):
+        if shape_id in self._data:
+            self._data[shape_id].update(options)
 
 
 class Entities:
