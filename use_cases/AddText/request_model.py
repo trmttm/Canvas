b4751dea1f0108a30e38cc4939b7457d2012cb65
trmttm <96355189+trmttm@users.noreply.github.com>
2022-02-21 01:46:34 +0400

    fix

--- use_cases/AddText/request_model.py
+++ use_cases/AddText/request_model.py
@@ -1,10 +1,11 @@
 from typing import Tuple
 
 
-def get_request_model(xy: Tuple[int, int] = (20, 20), text: str = 'text', font_size: int = 13,
+def get_request_model(xy: Tuple[int, int] = (20, 20), text: str = 'text', color='black', font_size: int = 13,
                       wh: Tuple[int, int] = (0, 0), text_rotation=0, tags=()):
     request_model = {'xy': xy,
                      'text': text,
+                     'color': color,
                      'font_size': font_size,
                      'wh': wh,
                      'text_rotation': text_rotation,
