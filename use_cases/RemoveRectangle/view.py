7d7502bd64862103a594f38dc08cb33ae75eed2c
trmttm <96355189+trmttm@users.noreply.github.com>
2022-02-22 13:21:59 +0400

    Reformat

--- use_cases/RemoveRectangle/view.py
+++ use_cases/RemoveRectangle/view.py
@@ -4,4 +4,5 @@ from interface_view import ViewABC
 def view_factory(app: ViewABC):
     def view_method(view_model: dict):
         app.remove_shape([view_model['shape_id']])
+
     return view_method
--- use_cases/SetBorderColor/view.py
+++ use_cases/SetBorderColor/view.py
@@ -4,4 +4,5 @@ from interface_view import ViewABC
 def view_factory(app: ViewABC):
     def view_method(view_model: dict):
         app.set_border_color(view_model)
+
     return view_method
