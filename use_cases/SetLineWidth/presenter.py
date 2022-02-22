da4de641564a7b466c4fad58528f317f9794ca53
trmttm <96355189+trmttm@users.noreply.github.com>
2022-02-22 15:07:07 +0400

    Type Hint

--- use_cases/SetLineWidth/presenter.py
+++ use_cases/SetLineWidth/presenter.py
@@ -1,7 +1,8 @@
 from use_cases.presenter import BasePresenter
+from use_cases.presenter_abc import PresenterABC
 from .view_model import create_view_model
 
 
-def presenter_factory() -> BasePresenter:
+def presenter_factory() -> PresenterABC:
     presenter = BasePresenter(create_view_model)
     return presenter
