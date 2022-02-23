f378b6f0b828464ffde0a4f5a7857b7f78917b3e
trmttm <96355189+trmttm@users.noreply.github.com>
2022-02-23 22:07:53 +0400

    Rectangle draw using mouse

--- use_cases/ChangeRectangleShape/test_use_case.py
+++ use_cases/ChangeRectangleShape/test_use_case.py
@@ -35,11 +35,12 @@ class MyTestCase(unittest.TestCase):
             shape_id = request.get('shape_id', None)
             if shape_id is not None:
                 x, y = request['x'], request['y']
-                request_model = {'shape_id': shape_id, 'coordinates_from': (20, 20), 'coordinates_to': (x, y)}
+                x0, y0 = request['coordinates'][0]
+                request_model = {'shape_id': shape_id, 'coordinates_from': (x0, y0), 'coordinates_to': (x, y)}
                 command.configure(**request_model)
                 command.execute()
 
-        test_app.configure_mouse(upon_mouse_click, test_app.mouse.is_left_click, {})
+        test_app.configure_mouse(upon_mouse_click, test_app.mouse.is_left_click, {'save click coordinate': True})
         test_app.configure_mouse(upon_mouse_drag, test_app.mouse.is_left_drag, {'shape_id': f'rect_{1}', })
 
         from Tests.test_methods import add_ten_rectangles
