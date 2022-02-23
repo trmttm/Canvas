7350dcdf14a4a4b1d76f314a12859afb218ce7f4
trmttm <96355189+trmttm@users.noreply.github.com>
2022-02-23 20:49:01 +0400

    get a single shape id from a tag

--- use_cases/MoveText/use_case.py
+++ use_cases/MoveText/use_case.py
@@ -9,7 +9,7 @@ class MoveText(UseCaseABC):
 
     def update_entities(self):
         shape_id_passed = self._configuration.get('shape_id')
-        shape_id = self._entities.texts.get_shape_ids_by_tag(shape_id_passed)[0]
+        shape_id = self._entities.texts.get_a_single_shape_id_by_tag(shape_id_passed)
         coordinates_from = self._entities.texts.get_xy(shape_id)
         delta_x = self._configuration.get('delta_x')
         delta_y = self._configuration.get('delta_y')
@@ -18,7 +18,7 @@ class MoveText(UseCaseABC):
 
     def create_response_model(self, coordinates_from: Tuple[int, int], *args, **kwargs):
         shape_id_passed = self._configuration.get('shape_id')
-        shape_id = self._entities.texts.get_shape_ids_by_tag(shape_id_passed)[0]
+        shape_id = self._entities.texts.get_a_single_shape_id_by_tag(shape_id_passed)
         x1, y1 = coordinates_from
         x2, y2 = self._entities.texts.get_xy(shape_id)
         delta_x, delta_y = x2 - x1, y2 - y1
