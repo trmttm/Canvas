2d409563e2d8ee6f5d24991763a946e651c9ab6d
trmttm <96355189+trmttm@users.noreply.github.com>
2022-02-23 16:00:42 +0400

    SelectShape does not present anything

--- use_cases/SelectShape/view.py
+++ use_cases/SelectShape/view.py
@@ -3,6 +3,6 @@ from interface_view import ViewABC
 
 def view_factory(app: ViewABC):
     def view_method(view_model: dict):
-        app.set_border_color(view_model)
+        pass  # Do nothing. Be open for higher layer to decide what to present / feedback.
 
     return view_method
--- use_cases/SelectShape/view_model.py
+++ use_cases/SelectShape/view_model.py
@@ -1,6 +1,3 @@
 def create_view_model(response_model):
-    # items = tuple((shape_id, 'red') for shape_id in response_model.get('contents', ()))
-    # view_model = dict(zip(range(len(items)), items))
-    shape_ids = response_model.get('contents', ())
-    view_model = dict(zip(shape_ids, tuple('red' for _ in shape_ids)))
+    view_model = response_model
     return view_model
